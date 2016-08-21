c1afd69ca40d58f7cbf713077230aa2a534c246c
Aurelijus Banelis <aurelijus@banelis.lt>
2016-08-21 21:23:52 +0300

    Own wrapper for SSH
    Old one did not had download from server.

--- cmd/sync/sync.go
+++ cmd/sync/sync.go
@@ -2,31 +2,21 @@ package main
 
 import (
 	"fmt"
-	"github.com/Auginte/go-monitoring/domain/common"
-	"gopkg.in/hypersleep/easyssh.v0"
+	"github.com/Auginte/go-monitoring/domain/sync"
 	"os"
-	"strings"
 )
-
-func testShh(ssh *easyssh.MakeConfig) {
-	response, err := ssh.Run("ps ax")
-	common.LogError(err)
-	fmt.Println(response)
-}
 
 func main() {
 	if len(os.Args) >= 3 {
-		parts := strings.SplitN(os.Args[1], "@", 2)
-		ssh := &easyssh.MakeConfig{
-			User:   parts[0],
-			Server: parts[1],
-			Key:    os.Args[2],
-			Port:   "22",
-		}
-		testShh(ssh)
+		ssh := sync.MakeSSH(os.Args[1], os.Args[2])
+		session := ssh.NewSession()
+		session.PipeStdError()
+		data := session.Run("ps aux")
+		fmt.Printf("|%s|\n", data)
+		ssh.Close()
 	} else {
 		fmt.Println("Tool to sync logs from LIVE to DEV machine")
 		fmt.Println("Usage:")
-		fmt.Println("\tsync user@example.com /.ssh/private.pem")
+		fmt.Println("\tsync user@example.com /home/user/.ssh/private.pem")
 	}
 }
